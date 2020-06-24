b4d766e8ccf6a55c8819104e6779319ef8268676
Shreya <shreyabarua2@gmail.com>
2020-06-24 19:42:35 +0000

    updating java servlet

--- resume-buddy/src/main/java/com/google/sps/ReviewerDataServlet.java
+++ resume-buddy/src/main/java/com/google/sps/ReviewerDataServlet.java
@@ -5,28 +5,43 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import java.util.ArrayList;
-import java.util.List;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.sps.Reviewer;
+import com.google.gson.Gson;
 
 /** Servlet that saves reviewer data from the form */
 @WebServlet("/reviewer-data")
 public class ReviewerDataServlet extends HttpServlet {
+
+    private Reviewer reviewer;
   
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Send the JSON as the response
+    response.setContentType("application/json");
+    String json = new Gson().toJson(reviewer);
+    response.getWriter().println(json);
 
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get the input from the form.
+    String fname = getParameter(request, "fname", "");
+    String lname = getParameter(request, "lname", "");
+    String email = getParameter(request, "email", "");
+    reviewer = new Reviewer(fname, lname, email);
+    Entity reviewerEntity = new Entity("Reviewer");
+    reviewerEntity.setProperty("first-name", fname);
+    reviewerEntity.setProperty("last-name", lname);
+    reviewerEntity.setProperty("email", email);
 
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(reviewerEntity);
   }
 
   /**
