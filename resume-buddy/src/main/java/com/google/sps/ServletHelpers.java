f6afd23c1bc49cee5d7e493f163e93e150b6ce81
Shreya <shreyabarua2@gmail.com>
2020-06-29 18:28:36 +0000

    adding utility class for servlets

---
+++ resume-buddy/src/main/java/com/google/sps/ServletHelpers.java
@@ -0,0 +1,18 @@
+package com.google.sps;
+
+import javax.servlet.http.HttpServletRequest;
+
+/* utility class dedicated to static helper functions for servlets*/
+public class ServletHelpers {
+
+  private ServletHelpers() {}
+
+  /**
+   * @return the request parameter, or the default value if the parameter was not specified by the
+   *     client
+   */
+  public static String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    return (value == null) ? defaultValue : value;
+  }
+}
--- resume-buddy/src/main/java/com/google/sps/servlets/ReviewerDataServlet.java
+++ resume-buddy/src/main/java/com/google/sps/servlets/ReviewerDataServlet.java
@@ -4,6 +4,7 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.gson.Gson;
+import com.google.sps.ServletHelpers;
 import com.google.sps.data.Reviewer;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -28,9 +29,9 @@ public class ReviewerDataServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
-    String fname = getParameter(request, "fname", "");
-    String lname = getParameter(request, "lname", "");
-    String email = getParameter(request, "email", "");
+    String fname = ServletHelpers.getParameter(request, "fname", "");
+    String lname = ServletHelpers.getParameter(request, "lname", "");
+    String email = ServletHelpers.getParameter(request, "email", "");
     reviewer = new Reviewer(fname, lname, email);
     Entity reviewerEntity = new Entity("Reviewer");
     reviewerEntity.setProperty("first-name", fname);
@@ -42,15 +43,6 @@ public class ReviewerDataServlet extends HttpServlet {
 
     /* TODO: add a response.sendRedirect() to wherever the user should
      * be navigated to next (either homepage or resume review page)
-     */ 
-  }
-
-  /**
-   * @return the request parameter, or the default value if the parameter was not specified by the
-   * client
-   */
-  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
-    String value = request.getParameter(name);
-    return (value == null) ? defaultValue : value;
+     */
   }
 }
