a0e9703e7f32c857ae9344da8d376cc2a157b367
Ani Machaidze <animachaidze@gmail.com>
2020-06-30 19:38:10 +0000

    Revert "Add Reviewee and Reviewer object files"
    This reverts commit 63030a32a6aaffe9fedd5f75bcb5577c9535c0d4.

--- resume-buddy/src/main/java/com/google/sps/data/Reviewer.java
+++ resume-buddy/src/main/java/com/google/sps/data/Reviewer.java
@@ -1,71 +1,15 @@
 package com.google.sps.data;
 
+/*TODO: add more information from form input into Reviewer*/
 public final class Reviewer {
 
   private final String firstName;
   private final String lastName;
   private final String email;
-  private final String school;
-  private final String fieldOfWork;
-  private final String company;
-  private final String highestLevelOfEducation;
-  private final String yearsOfWorkExperience;
 
   public Reviewer(String fname, String lname, String email) {
     this.firstName = fname;
     this.lastName = lname;
     this.email = email;
-    this.school = "";
-    this.company = "";
-    this.fieldOfWork = "";
-    this.highestLevelOfEducation = "";
-    this.yearsOfWorkExperience = "";
-  }
-
-  public Reviewer(
-      String firstName,
-      String lastName,
-      String email,
-      String school,
-      String company,
-      String fieldOfWork,
-      String highestLevelOfEducation,
-      String yearsOfWorkExperience) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.school = school;
-    this.company = company;
-    this.fieldOfWork = fieldOfWork;
-    this.highestLevelOfEducation = highestLevelOfEducation;
-    this.yearsOfWorkExperience = yearsOfWorkExperience;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public String getSchool() {
-    return school;
-  }
-
-  public String getFieldOfWork() {
-    return fieldOfWork;
-  }
-
-  public String getCompany() {
-    return company;
-  }
-
-  public String getYearsOfWorkExperience() {
-    return yearsOfWorkExperience;
   }
 }
