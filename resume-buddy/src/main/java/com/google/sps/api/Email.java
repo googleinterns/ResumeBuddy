3904948bfca2db5a2af9bac361dff70803213a3b
Ani Machaidze <animachaidze@gmail.com>
2020-07-10 21:19:26 +0000

    Emails can have attachments

--- resume-buddy/src/main/java/com/google/sps/api/Email.java
+++ resume-buddy/src/main/java/com/google/sps/api/Email.java
@@ -5,25 +5,38 @@ import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.Message;
 import com.google.sps.Consts;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /** Class responsible to create an email and send it to recipient */
 public class Email {
 
   private Email() {};
 
-  public static void sendEmail(String to, String subjectLine, String text) {
+  public static void sendEmail(String to, String subjectLine, String text, File file)
+      throws IOException {
     try {
       Gmail service = GmailService.getGmailService();
-      MimeMessage mimeMessage = createEmail(to, Consts.FROM, subjectLine, text);
-      Message message = createMessageWithEmail(mimeMessage);
-      message = service.users().messages().send(Consts.FROM, message).execute();
+      MimeMessage mimeMessage = null;
 
+      if (file == null) {
+        mimeMessage = createEmail(to, Consts.FROM, subjectLine, text);
+      } else {
+        mimeMessage = createEmailWithAttachment(to, Consts.FROM, subjectLine, text, file);
+      }
+
+      sendMessage(service, Consts.FROM, mimeMessage);
     } catch (Exception e) {
       System.err.println("Could not send the email");
     }
@@ -68,6 +81,66 @@ public class Email {
     String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
     Message message = new Message();
     message.setRaw(encodedEmail);
+    return message;
+  }
+
+  /**
+   * Creates a MimeMessage using the parameters provided.
+   *
+   * @param to Email address of the receiver.
+   * @param from Email address of the sender, the mailbox account.
+   * @param subject Subject of the email.
+   * @param bodyText Body text of the email.
+   * @param file Path to the file to be attached.
+   * @return MimeMessage to be used to send email.
+   * @throws MessagingException
+   */
+  public static MimeMessage createEmailWithAttachment(
+      String to, String from, String subject, String bodyText, File file)
+      throws MessagingException, IOException {
+    Properties props = new Properties();
+    Session session = Session.getDefaultInstance(props, null);
+
+    MimeMessage email = new MimeMessage(session);
+
+    email.setFrom(new InternetAddress(from));
+    email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+    email.setSubject(subject);
+
+    MimeBodyPart mimeBodyPart = new MimeBodyPart();
+    mimeBodyPart.setContent(bodyText, "text/plain");
+
+    Multipart multipart = new MimeMultipart();
+    multipart.addBodyPart(mimeBodyPart);
+
+    mimeBodyPart = new MimeBodyPart();
+    DataSource source = new FileDataSource(file);
+
+    mimeBodyPart.setDataHandler(new DataHandler(source));
+    mimeBodyPart.setFileName(file.getName());
+
+    multipart.addBodyPart(mimeBodyPart);
+    email.setContent(multipart);
+
+    return email;
+  }
+
+  /**
+   * Sends an email from the user's mailbox to its recipient.
+   *
+   * @param service Authorized Gmail API instance.
+   * @param userId User's email address. The special value "me" can be used to indicate the
+   *     authenticated user.
+   * @param emailContent Email to be sent.
+   * @return The sent message
+   * @throws MessagingException
+   * @throws IOException
+   */
+  public static Message sendMessage(Gmail service, String userId, MimeMessage emailContent)
+      throws MessagingException, IOException {
+    Message message = createMessageWithEmail(emailContent);
+    message = service.users().messages().send(userId, message).execute();
+
     return message;
   }
 }
