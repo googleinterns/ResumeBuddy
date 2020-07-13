e0f94dde371372b5fe533419def05bf0cc27a3ef
Shayla <sesexton@google.com>
2020-07-13 14:39:23 +0000

    spell retreival correctly smh

--- resume-buddy/src/main/java/com/google/sps/servlets/PDFRetreivalServlet.java
+++ resume-buddy/src/main/java/com/google/sps/servlets/PDFRetreivalServlet.java
@@ -5,10 +5,12 @@ You may need to write a new servlet and have `resume-review.html` do a POST and 
 can be named something like `PdfRetrievalServlet` `resume-review.html` will need to POST the reviewee's 
 identity. Then `PdfRetrievalServlet` will need to serve the reviewee's PDF to `resume-review.html`, 
 which will then supply it to Adobe DC Viewer element 
+
+Basically, resume review calls this, the do post will grab the users identity, and their resume, and the do get will return the resume url to the adobr viewer element
 */
 
-@WebServlet("pdf-retrival")
-public class PdfRetrievalServlet extends HttpServlet {
+@WebServlet("pdf-retreival")
+public class PdfRetreivalServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
