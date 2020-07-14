55d3d66ee0191ff3266ada9bc8cd82adcdae9228
Ani Machaidze <animachaidze@gmail.com>
2020-07-14 16:57:41 +0000

    Add reviewServelt

---
+++ resume-buddy/src/main/java/com/google/sps/servlets/ReviewServlet.java
@@ -0,0 +1,48 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.sps.data.ReviewStatus;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/review-done")
+public class ReviewServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {}
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    String reviewerEmail = userService.getCurrentUser().getEmail();
+
+    Query query = new Query("Match");
+    Filter reviewerFilter = new FilterPredicate("reviewer", FilterOperator.EQUAL, reviewerEmail);
+    query.setFilter(reviewerFilter);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+    String revieweeEmail = null;
+
+    for (Entity entity : results.asIterable()) {
+      entity.setProperty("status", ReviewStatus.DONE.toString());
+      revieweeEmail = (String) entity.getProperty("reviewee");
+    }
+
+    System.out.println("people --- " + revieweeEmail + " " + reviewerEmail);
+
+    response.sendRedirect("/index.html");
+  }
+}
