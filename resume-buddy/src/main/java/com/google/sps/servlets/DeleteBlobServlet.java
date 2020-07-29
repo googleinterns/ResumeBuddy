c76884c70283bcb1c7030025b72f77a48b7612ba
Shayla <sesexton@google.com>
2020-07-29 15:37:46 +0000

    Olivia edits

--- resume-buddy/src/main/java/com/google/sps/servlets/DeleteBlobServlet.java
+++ resume-buddy/src/main/java/com/google/sps/servlets/DeleteBlobServlet.java
@@ -1,3 +1,4 @@
+import com.google.appengine.api.blobstore.BlobInfoFactory;
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
@@ -19,19 +20,7 @@ public class DeleteBlobServlet extends HttpServlet {
     BlobKey blobKey = new BlobKey(resumeBlobKey);
     blobstoreService.delete(blobKey);
 
-    // check if blob is empty (not sure if this will work, I dont know what the blobkey is being set
-    // too after its deleted)
-    if (blobKey == null) {
-      isBlobDeleted = true;
-    }
-
-    // set the status message corresponding to wheter its true or false
-    if (isBlobDeleted) {
-      response.setStatus(200); // works
-      // response.setMessage("The blob has been deleted!");
-    } else {
-      response.setStatus(400); // error
-      // response.statusText("The blob is not been deleted.");
-    }
+    BlobInfoFactory blobInfoFactory = new BlobInfoFactory();
+    isBlobDeleted = blobInfoFactory.loadBlobInfo(blobKey) == null;
   }
 }
